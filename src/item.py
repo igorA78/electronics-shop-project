8db25209c92347c2dccacf73246fbc7ffaf93fe3
IgorK78 <i.kolegov78@gmail.com>
2023-06-05 22:17:35 +0300

    edit hw-2

--- src/item.py
+++ src/item.py
@@ -1,3 +1,5 @@
+import csv
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,7 +15,11 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        pass
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        self.all.append(self)
+
 
     def calculate_total_price(self) -> float:
         """
@@ -21,10 +27,30 @@ class Item:
 
         :return: Общая стоимость товара.
         """
-        pass
+        return self.price * self.quantity
 
     def apply_discount(self) -> None:
         """
         Применяет установленную скидку для конкретного товара.
         """
-        pass
+        self.price *= self.pay_rate
+
+    @property
+    def name(self) -> str:
+        return self.__name
+
+    @name.setter
+    def name(self, name: str) -> None:
+        self.__name = name if len(name) <= 10 else 'Exception: Длина наименования товара превышает 10 символов.'
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        Item.all.clear()
+        with open('../src/items.csv', 'r') as file:
+            items = csv.DictReader(file)
+            for row in items:
+                cls(row['name'], row['price'], row['quantity'])
+
+    @staticmethod
+    def string_to_number(number: str) -> int:
+        return int(float(number))
